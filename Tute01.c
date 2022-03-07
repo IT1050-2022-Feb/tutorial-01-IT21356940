20c152ccc10c86fa98f2342921fea7b1ad0308df
Tharupathi anjula <99165709+IT21356940@users.noreply.github.com>
2022-03-07 10:09:52 +0000

    tutorial 1 is completed

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main() {
+  int mark1 , mark2; //define variables
+	float avg;
+	
+	printf("Enter the first mark :");  //Ask user to input mark1
+	scanf("%d" , &mark1);
+	
+	printf("Enter the second mark :"); //ask uer to input mark2
+	scanf("%d" , &mark2);
+	
+	avg = (float)(mark1 + mark2)/2;   //Calculation for average
+	
+	printf("Average : %.2f" , avg);   //printing average
+	
+	return 0;  //End of the process
   
-  return 0;
+ 
 }
 
